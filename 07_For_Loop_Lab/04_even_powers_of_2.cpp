c292619dcbc362b5835fdad780c3bd142de09d8c
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-10-09 10:07:16 +0300

    added tasks from For Loop Lab

---
+++ 07_For_Loop_Lab/04_even_powers_of_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i <= n; i += 2) {
+        cout << pow(2, i) << endl;
+    }
+
+    return 0;
+}
